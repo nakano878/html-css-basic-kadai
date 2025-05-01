dd4f1e3918fae4869fc537eb76dc388ae7b3a21d
nakano878 <karasuka0321@icloud.com>
2025-05-01 14:52:02 +0900

    Add files via upload

---
+++ kadai_012/crud/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    myText = models.TextField(
+        verbose_name='',
+        blank=True,
+        null=True,
+        max_length=1000,
+    )
+
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.ImageField(blank=True, default='noImage.png')
+
+    def __str__(self):
+        return self.name
+    #新規作成・編集完了時のリダイレクト先
+    def get_absolute_url(self):
+        return reverse('list')
